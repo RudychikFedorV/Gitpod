d47e66d131666ecfb00cd1e3da255bba7ea2890b
RudychikFedorV <rudychik.fedor@gmail.com>
2024-01-02 22:20:30 +0200

    commit

---
+++ src/components/AlwaysReady/AlwaysReady.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="alwaysReady">
+    <div class="container">
+      <div class="alwaysReady__block">
+        <div class="alwaysReady__title">
+          Always
+          <div>Ready to Code.</div>
+        </div>
+        <div class="alwaysReady__info">
+          Spin up fresh, automated dev environments
+          <div>for each task, in the cloud, in seconds.</div>
+        </div>
+        <div class="alwaysReady__section">
+          <div class="alwaysReady__icons">
+            <div class="alwaysReady__button">Try Now</div>
+            <div class="alwaysReady__gitlab">
+              <img src="@/assets/AlwaysReady/gitlab.svg" alt="Gitlab" />
+            </div>
+            <div class="alwaysReady__gitHub">
+              <img src="@/assets/AlwaysReady/gitHub.svg" alt="GitHub" />
+            </div>
+            <div class="alwaysReady__bitbucket">
+              <img src="@/assets/AlwaysReady/bitbucket.svg" alt="Bitbucket" />
+            </div>
+          </div>
+          <div class="alwaysReady__text">
+            Open a workspace.
+            <div>Start from any Git context.</div>
+          </div>
+        </div>
+      </div>
+      <div class="alwaysReady__foto">
+        <img src="@/assets/AlwaysReady/foto.png" alt="Logo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <HeaderBlock />
+    <AlwaysReady />
+  </div>
+</template>
+
+<script>
+import HeaderBlock from "@/components/HeaderBlock/HeaderBlock.vue";
+import AlwaysReady from "@/components/AlwaysReady/AlwaysReady.vue";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+  methods: {},
+  components: { HeaderBlock, AlwaysReady },
+};
+</script>
